14dc33bcb2b1375c13266b48a183c855729f93ad
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2014-03-24 16:08:02 +0100

    Changed clone.php once again

--- clone.php
+++ clone.php
@@ -1,11 +1,13 @@
 <?php 
-
-	$command = 'touch test.txt';
+	
+	// clone git repo
+	$command = 'git clone https://github.com/kr15h/github-hook-test.git .';
 	$output = shell_exec( $command );
 	echo $output; 
 
-	$command = 'git pull';
+	// delete .git directory to save space
+	$command = 'rm -rf .git';
 	$output = shell_exec( $command );
 	echo $output; 
-
+	
 ?>
