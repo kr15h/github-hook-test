eb3a10b8e447de840fc4ff8d8041c5eb0eba304b
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2014-03-24 16:21:31 +0100

    Lets test the Github hook!

--- clone.php
+++ clone.php
@@ -3,16 +3,16 @@
 	// delete all
 	$command = 'rm -rf *';
 	$output = shell_exec( $command );
-	echo $output; 
+	echo $output."\n"; 
 
 	// clone git repo
 	$command = 'git clone https://github.com/kr15h/github-hook-test.git .';
 	$output = shell_exec( $command );
-	echo $output; 
+	echo $output."\n"; 
 
 	// delete .git directory to save space
 	$command = 'rm -rf .git';
 	$output = shell_exec( $command );
-	echo $output; 
+	echo $output."\n"; 
 
 ?>
