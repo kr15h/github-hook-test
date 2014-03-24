62c0675a6e49a13107421a45a3ae600079b481af
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2014-03-24 16:15:57 +0100

    Before we clone, delete all

--- clone.php
+++ clone.php
@@ -1,5 +1,10 @@
 <?php 
 	
+	// delete all
+	$command = 'rm -rf *';
+	$output = shell_exec( $command );
+	echo $output; 
+
 	// clone git repo
 	$command = 'git clone https://github.com/kr15h/github-hook-test.git .';
 	$output = shell_exec( $command );
@@ -9,5 +14,5 @@
 	$command = 'rm -rf .git';
 	$output = shell_exec( $command );
 	echo $output; 
-	
+
 ?>
