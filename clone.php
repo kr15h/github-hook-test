196801aedee5ea7c50be7082ea412d61f8c7bf52
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2014-03-24 15:40:50 +0100

    Another change

--- clone.php
+++ clone.php
@@ -4,8 +4,7 @@ $get_passw = "cloneplease";
 
 if ( $_GET['passw'] == "cloneplease" ) {
 	// clone from git repo
-	$command = 'git clone https://github.com/kr15h/github-hook-test.git .';
-	exec( $command );
+	$command = 'git pull';
 } 
 
 ?>
