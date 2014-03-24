c0f342fad8a599657335fbb4dd0800edfe38d6e3
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2014-03-24 15:50:26 +0100

    Added touch test to clone.php

--- clone.php
+++ clone.php
@@ -1,6 +1,9 @@
 <?php 
 
-	// clone from git repo
+	$command = 'touch test.txt';
+	$output = shell_exec( $command );
+	echo $output; 
+
 	$command = 'git pull';
 	$output = shell_exec( $command );
 	echo $output; 
